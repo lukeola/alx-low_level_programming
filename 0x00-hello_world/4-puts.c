5ab7fc7a1011d56bb0d2a700b39d2bd8c5756c35
Luke Olawale <lukeolawale4@gmail.com>
2022-06-30 14:32:07 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
- * main -"Programming is like building a multilingual puzzle"
- * Return: 0 if exited properly, else non-zero 
+ * main - Entry point
+ * Return: Always 0 (sucess)
  */
-int main(void)
+ int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
- * main - prints the phrase with proper grammar
- * but the outcome is a piece of art,
- * Return: 0 if exited properly, non-zero otherwise   
+ * main - print the grammer text
+ * Return: Always 0 (success)
  */
-int main(void)
+ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);

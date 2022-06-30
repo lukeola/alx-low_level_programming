173acae1f1a48adf8626ab801487f9a2f843a9e2
Luke Olawale <lukeolawale4@gmail.com>
2022-06-30 14:08:07 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle
+ * main -"Programming is like building a multilingual puzzle
  * Return: 0 if exited properly, else non-zero. 
  */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

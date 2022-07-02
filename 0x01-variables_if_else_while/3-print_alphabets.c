a587f73205b3939adb02619e8e72a5288148be8d
Luke Olawale <lukeolawale4@gmail.com>
2022-07-02 00:02:45 +0000

    Variables

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,16 +8,10 @@
 int main(void)
 {
 	int ch;
-
 	for (ch = 'a'; ch <= 'z'; ch++)
-
 		putchar(ch);
-
 	for (ch = 'A'; ch <= 'Z'; ch++)
-
-		putchar(ch);
-	
+	putchar(ch);
 	putchar('\n');
-	
 	return (0);
 }

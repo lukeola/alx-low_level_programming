0d7cc70f29ef9a50a65d76cb25d91a1c4ac5ded3
Luke Olawale <lukeolawale4@gmail.com>
2022-07-28 13:27:18 -0700

    malocs

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,21 +1,52 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
+
 /**
- * _strlen - Finds the length of a string.
- * @str: The string to be measured.
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
  *
- * Return: The length of the string.
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-int _strlen(char *str)
+int is_digit(char *s)
 {
-	int i;
+	int i = 0;
 
-	for(i = 0; str[i]; i++)
-		;
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 	return (i);
 }
+
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
 /**
  * main - multiplies two positive numbers
  * @argc: number of arguments
@@ -25,23 +56,44 @@ int _strlen(char *str)
  */
 int main(int argc, char *argv[])
 {
-	unsigned long int a, b, res; 
-	
-	int len_a, len_b;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	len_a = _strlen(argv[1]);
-	len_b = _strlen(argv[2]);
-
-	for (i = 0; i < )
-
-	if (argc != 3)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		printf("Error\n");
-		return (98);
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-
-	res = mul(a, b);
-	printf("%d", res);
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
+	return (0);
 }

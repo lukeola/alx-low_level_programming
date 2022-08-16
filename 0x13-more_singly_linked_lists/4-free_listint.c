a9d27ef7b0d7537697ed8f253792150e1b70f8bd
Luke Olawale <lukeolawale4@gmail.com>
2022-08-16 09:20:44 -0700

    More stingly Linked Lists

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while ((temp = head) != NULL)
+	{
+		head = head->next;
+		free(temp);
+	}
+}
